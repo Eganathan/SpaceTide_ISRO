484a46588046115782c3b32bf0e77e4cf4d0adea
eganathan.r <eganathan.r@zohocorp.com>
2023-12-05 18:09:12 +0530

    chore: ui correction

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,34 @@ D
 app/build.gradle.kts,d/2/d2a4383452aad84a18429a5844d31330a012a9e6
 @
 build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
+|
+Lapp/src/main/java/dev/eknath/spacetide_isro/notes/Dependency Setup Notes.txt,8/5/856a8743d6393506507ae254afa4d29d0e917ada
+u
+Eapp/src/main/java/dev/eknath/spacetide_isro/data/di/DatabaseModule.kt,7/2/72820e87670d652797529b11d2fec4c544519ead
+…
+Uapp/src/main/java/dev/eknath/spacetide_isro/data/sources/remote/util/MissionStatus.kt,7/c/7c3b5ae3413cb47af660799c531945e25d937f3c
+{
+Kapp/src/main/java/dev/eknath/spacetide_isro/data/sources/remote/Adapters.kt,b/6/b6a4ff828b16bd6854f6353130b4adc6ff7dea69
+o
+?app/src/main/java/dev/eknath/spacetide_isro/ISRO_Application.kt,f/0/f0801292648f316397730e7c5bcac50ab255ff80
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+t
+Dapp/src/main/java/dev/eknath/spacetide_isro/data/di/NetworkModule.kt,1/e/1e8dfcb13daee3674ca5c92b5008efed5732d43e
+w
+Gapp/src/main/java/dev/eknath/spacetide_isro/data/di/RepositoryModule.kt,9/b/9b1d239ad37fa75a56481c9189929acae3b387d9
+‡
+Wapp/src/main/java/dev/eknath/spacetide_isro/data/repository/SpacecraftRepositoryImpl.kt,1/0/1028d3eaabc4217a033e0aab9ea89593655aa12d
+…
+Uapp/src/main/java/dev/eknath/spacetide_isro/domain/repository/SpacecraftRepository.kt,c/2/c2d7464e0421ea34d3e881f529d889c696cb0b3b
+ˆ
+Xapp/src/main/java/dev/eknath/spacetide_isro/data/sources/remote/api/SpacecraftService.kt,0/d/0d28c6695b9712b4e78bc879b9e908a51ef2e6ec
+€
+Papp/src/main/java/dev/eknath/spacetide_isro/presentation/SpaceCraftsViewModel.kt,f/3/f30214cace5a5d682f14c5ac2632a9f102cc2b1b
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+‰
+Yapp/src/main/java/dev/eknath/spacetide_isro/data/sources/remote/model/SpacecraftEntity.kt,4/7/47616711a03cdb6cc7bab79e7540ce07d9d3619c
+k
+;app/src/main/java/dev/eknath/spacetide_isro/MainActivity.kt,c/2/c2356783a7067bb25b00d36f2de4b5c2823df8a1
--- app/src/main/java/dev/eknath/spacetide_isro/MainActivity.kt
+++ app/src/main/java/dev/eknath/spacetide_isro/MainActivity.kt
@@ -3,9 +3,15 @@ package dev.eknath.spacetide_isro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import dev.eknath.spacetide_isro.presentation.SpaceCraftsViewModel
@@ -43,7 +50,23 @@ class MainActivity : ComponentActivity() {
                             }
                         } else {
                             items(items = content.value.orEmpty()) {
-                                Text(text = "${it.name}")
+                                Card(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(20.dp)
+                                        .heightIn(min = 80.dp),
+                                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+                                ) {
+                                    Column(modifier = Modifier.padding(5.dp)) {
+                                        Text(text = it.name, style = MaterialTheme.typography.displaySmall)
+                                        Text(text = "orbitType:${it.orbitType}")
+                                        Text(text = "Date: ${it.launchDate}")
+                                        Text(text = "application: ${it.application}")
+                                        Text(text = "vechile: ${it.launchVehicle}")
+                                        Text(text = "status :${it.missionStatus}")
+                                    }
+
+                                }
                             }
                         }
                     }
